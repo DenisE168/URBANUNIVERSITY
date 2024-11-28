7e8b9bf4ffc9a6a91a414116791233136d4fcf16
DenisE168 <denisetobaev@gmail.com>
2024-11-28 18:08:10 +0800

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -11,13 +11,11 @@ def is_contains(string, list_to_search):
     string = str(string).lower()
     list_to_search = list(list_to_search)
     count_calls()
+    result = False
     for i in range(len(list_to_search)):
         if str(list_to_search[i]).lower() == string:
             result = True
-            break
-        else:
-            result = False
-            continue
+            break 
     return result
 print(string_info('Piroshok'))
 print(string_info('Forest'))
